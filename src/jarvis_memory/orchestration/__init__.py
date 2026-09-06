3027bb7504547b64c18ac67415ea49ea98b1cf7b
badithalanitinpreetham-droid <badithalanitinpreetham@gmail.com>
2026-09-07 03:00:22 +0530

    Export Hermes registry from orchestration package

--- src/jarvis_memory/orchestration/__init__.py
+++ src/jarvis_memory/orchestration/__init__.py
@@ -2,16 +2,18 @@
 
 Jarvis does not replace Hermes' tools, Bots, subagents or Kanban. These
 modules decide how those existing Hermes capabilities should be organised
-for a user goal and what context should be supplied to the workers.
+for a user goal and what bounded context should be supplied to workers.
 """
 
 from .context import build_context_packet
 from .experience import ExperienceSummary, summarize_experience
 from .organisation import AgentAssignment, OrganisationPlanner, OrganisationPlan
+from .registry import HermesRegistry
 
 __all__ = [
     "AgentAssignment",
     "ExperienceSummary",
+    "HermesRegistry",
     "OrganisationPlan",
     "OrganisationPlanner",
     "build_context_packet",
